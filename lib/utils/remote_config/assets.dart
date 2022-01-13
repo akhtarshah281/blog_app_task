34fe0060016b4d00304231aaa73cbd2b803b36f6
Muhammad Faizan <muhammad@tentwenty.me>
2022-01-13 15:04:06 +0500

    files moved to remote_config

---
+++ lib/utils/remote_config/assets.dart
@@ -0,0 +1,23 @@
+class Assets {
+  ///base path
+  static const String baseIconPath = 'assets/icons/';
+  static const String baseImagePath = 'assets/images/';
+
+  ///icons
+  static const String cross = baseIconPath + 'cross.svg';
+  static const String tick = baseIconPath + 'tick.svg';
+
+  ///images
+  static const String logo = baseImagePath + 'logo.png';
+  static const String food = baseImagePath + 'food.png';
+  static const String press = baseImagePath + 'press.png';
+  static const String vip = baseImagePath + 'vip.png';
+  static const String creativeHub = baseImagePath + 'creative_hub.png';
+  static const String horology = baseImagePath + 'horology.png';
+  static const String masterClass = baseImagePath + 'master_class.png';
+  static const String visitor = baseImagePath + 'visitor.png';
+  static const String scannerLabelBg = baseImagePath + 'scanner_label_bg.png';
+  static const String logout = baseImagePath + 'logout_image.png';
+  static const String cameraRect = baseImagePath + 'camera_rect.png';
+  static const String watchBg = baseImagePath + 'watch_bg.png';
+}
