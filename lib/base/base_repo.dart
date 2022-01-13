6fa03ee316c99eb6c7620a434e17375051b7ee75
Muhammad Faizan <muhammad@tentwenty.me>
2022-01-13 13:09:19 +0500

    Get it registeration with depends on

--- lib/base/base_repo.dart
+++ lib/base/base_repo.dart
@@ -1,6 +1,12 @@
+import 'package:blog_app_task/data/app_database.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 class BaseRepo{
 
+AppDatabase database = GetIt.I.get<AppDatabase>();
 
+static Future<AppDatabase> initDB()async{
+  return await $FloorAppDatabase.databaseBuilder('app_database.db').build();
+}
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,38 @@
+import 'package:blog_app_task/base/base_repo.dart';
+import 'package:blog_app_task/data/app_database.dart';
+import 'package:blog_app_task/data/repositories/add_post/add_post_repo.dart';
+import 'package:blog_app_task/data/repositories/add_post/add_post_repo_imp.dart';
+import 'package:blog_app_task/data/repositories/forgot/forgot_repo.dart';
+import 'package:blog_app_task/data/repositories/forgot/forgot_repo_imp.dart';
+import 'package:blog_app_task/data/repositories/home/home_repo_imp.dart';
+import 'package:blog_app_task/data/repositories/login/login_repo.dart';
+import 'package:blog_app_task/data/repositories/login/login_repo_imp.dart';
+import 'package:blog_app_task/data/repositories/register/register_repo.dart';
+import 'package:blog_app_task/data/repositories/register/register_repo_imp.dart';
+import 'package:blog_app_task/data/repositories/update_post/update_repo.dart';
+import 'package:blog_app_task/data/repositories/update_post/update_repo_imp.dart';
 import 'package:blog_app_task/routeGenerator.dart';
 import 'package:blog_app_task/ui/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 
-main () async {
+import 'data/repositories/home/home_repo.dart';
 
-  runApp(MaterialApp(onGenerateRoute: RouteGenerator,
-  initialRoute: SplashScreen.routeName,));
+main() async {
+  GetIt.I.registerSingletonAsync(() => BaseRepo.initDB());
+  GetIt.I.registerSingletonWithDependencies<AddPostRepo>(() => AddPostRepoImp(),
+      dependsOn: [AppDatabase]);
+  GetIt.I.registerSingletonWithDependencies<HomeRepo>(() => HomeRepoImp(),
+      dependsOn: [AppDatabase]);
+  GetIt.I.registerSingletonWithDependencies<LoginRepo>(() => LoginRepoImp(),
+      dependsOn: [AppDatabase]);
+  GetIt.I.registerSingletonWithDependencies<RegisterRepo>(
+      () => RegisterRepoImp(),
+      dependsOn: [AppDatabase]);
+  GetIt.I.registerSingletonWithDependencies<UpdateRepo>(() => UpdateRepoImp(),
+      dependsOn: [AppDatabase]);
+  GetIt.I.registerSingletonWithDependencies<ForgotRepo>(() => ForgotRepoImp(),
+      dependsOn: [AppDatabase]);
+  runApp(MaterialApp(
+      onGenerateRoute: RouteGenerator, initialRoute: SplashScreen.routeName));
 }
