47a6fe49707222810b9faa88357cdf4b6b3650eb
Muhammad Faizan <muhammad@tentwenty.me>
2022-01-13 12:09:22 +0500

    profile image

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:blog_app_task/routeGenerator.dart';
 import 'package:blog_app_task/ui/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 main () async {
 
   runApp(MaterialApp(onGenerateRoute: RouteGenerator,
   initialRoute: SplashScreen.routeName,));
-
 }
